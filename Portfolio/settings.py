fac4415112cacc61622cae880676411eb051c603
Maria Gichomo <mariagichomo35@gmail.com>
2023-05-16 16:05:15 +0300

    Added new files

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
