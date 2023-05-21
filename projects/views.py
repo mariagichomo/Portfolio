478ef36f366b99f2335445d1acf9f8778cb257df
Maria Gichomo <mariagichomo35@gmail.com>
2023-05-21 23:11:34 +0300

    Added code to views.py

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from project.models import Project
 
-# Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+
