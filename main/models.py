7cf5e023e36a82fc7a16aa4638a23c114db87b4a
Maria Gichomo <mariagichomo35@gmail.com>
2023-06-08 13:00:09 +0300

    trial

--- main/models.py
+++ main/models.py
@@ -34,11 +34,77 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
     
+class ContactProfile(models.Model):
 
-# Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    class Meta:
+        verbose_name_plural = "Contact Profiles"
+        verbose_name = "Contact Profile"
+        ordering = ["timestamp"]
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(verbose_name="Email")
+    message= models.TextField(verbose_name="Message")
+
+    def __str__(self):
+       return f'{self.name}'
+        
+class Testimonial(models.Model):
+
+    class Meta:
+       verbose_name_plural = "Testimonials"
+       verbose_name = "Testimonial"
+       ordering = ["name"]
+
+
+    thumbnail = models.ImageField(blank=True, null=True, upload_to="testimonials")
+    name = models.CharField(max_length=200, blank=True,null=True)
+    role= models.CharField(max_length=200, blank=True,null=True)
+    quote= models.CharField(max_length=500, blank=True,null=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+         return self.name
+    
+    class Media(models.Model):
+        
+        class Meta:
+            verbose_name_plural = "Media Files"
+            verbose_name = "Media"
+            ordering = ["name"]
+    
+
+    image = models.ImageField(blank=True, null=True, upload_to="media")
+    url = models.URLField(blank=True, null=True)
+    name= models.CharField(max_length=200, blank=True,null=True)
+    is_image = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.url:
+            self.is_image = False
+        super(Media, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
+    
+class Portfolio(models.Model):
+
+   class Meta:
+       verbose_name_plural = "Portfolio Profiles"
+       verbose_name = "Portfolio"
+       ordering = ["name"]
+
+
+
+
+    
+
+
+     
+        
+    
+        
+        
+
+
+    
 
